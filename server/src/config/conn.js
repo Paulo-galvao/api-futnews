c6e0ec5d747c7971bb3026f9fcd66708b150a5ec
Paulo-galvao <paulogalvaoj21@gmail.com>
2025-03-19 13:55:38 -0300

    api: finalizando

--- server/src/config/conn.js
+++ server/src/config/conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDatabase() {
     try {
-        await mongoose.connect(process.env.MONGODB);
+        await mongoose.connect("mongodb+srv://paulogalvaoj21:paulo123@cluster0.uxez3.mongodb.net/breaking-news");
         console.log("Connected to Mongo Database");
     } catch (error) {
         console.log(error.message);
